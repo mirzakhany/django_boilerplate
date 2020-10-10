0e194c314c4e19910318f8a0f66f0adfe13977fd
Saeed Ghareh Daghi <saeed.gharehdaghi@kpn.com>
2020-10-10 16:46:53 +0200

    OPT: change setting structure, add requirements.txt and upgrade docker

--- src/proj/settings/base.py
+++ src/proj/settings/base.py
@@ -17,6 +17,9 @@ from django.utils.translation import ugettext_lazy as _
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+def ENV_SETTING(key, default):
+    import os
+    return os.environ.get(key, default)
 
 if os.environ.get("ENABLE_SENTRY") == "True":
 
@@ -86,8 +89,7 @@ ROOT_URLCONF = 'proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -95,10 +97,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-            "loaders": [
-                "django.template.loaders.filesystem.Loader",
-                "django.template.loaders.app_directories.Loader",
             ],
         },
     },
---
+++ src/proj/settings/dev.py
@@ -0,0 +1,32 @@
+from .base import *
+
+DEBUG = (ENV_SETTING('DEBUG', 'true') == 'true')
+TEMPLATE_DEBUG = (ENV_SETTING('TEMPLATE_DEBUG', 'true') == 'true')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+# Disable caching while in development
+CACHES = {
+    'default': {
+        'BACKEND': ENV_SETTING('CACHE_BACKEND',
+            'django.core.cache.backends.dummy.DummyCache')
+    }
+}
+
+# Add SQL statement logging in development
+if (ENV_SETTING('SQL_DEBUG', 'false') == 'true'):
+    LOGGING['loggers']['django.db'] = {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': False
+    }
+
