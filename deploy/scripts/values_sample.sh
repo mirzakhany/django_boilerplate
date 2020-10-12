bc6de198f95eb0495c975c4f0c20815af7a312e6
mohsen <mohsen.mkh88@gmail.com>
2020-10-12 08:58:40 +0200

    NEW: build scripts

---
+++ deploy/scripts/values_sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# DO NOT PUSH THIS FILE !!!
+
+# this is a sample setting file to set required environment variables to build docker images. 
+
+export DOCKER_REGISTRY="index.docker.io";
+export DOCKER_USER="";
+export DOCKER_PASS="";
+
+export BASE_IMAGE_REPO="mirzakhani/django_boilerplate_base";
+export IMAGE_REPO="mirzakhani/django_boilerplate";
